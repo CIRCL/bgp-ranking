2db60101c7aea66f9b573c54465615ba0af7ff2a
Raphaël Vinot <raphael@vinot.info>
2010-02-07 15:32:59 +0100

    add a RIS-Whois parser, refactorization (class) and the ranking gives now the description (name) of the AS

--- getASNFromIp.py
+++ getASNFromIp.py
@@ -1,59 +1,61 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from socket import *
-import re
 import urllib2
-
-
-risServer = 'riswhois.ripe.net'
-whoisPort = 43
-
 ipListURL ='http://www.dshield.org/feeds/topips.txt'
 response = urllib2.urlopen(ipListURL)
 ipList = response.read()
-ips = re.findall('([0-9.]+).+',ipList)
 
-def ASNsofIPs(ips):
-  s = socket(AF_INET, SOCK_STREAM)
-  s.connect((risServer,whoisPort))
-  s.recv(1024)
+from socket import *
+import re
+from whoisParser import WhoisEntry
+from itertools import groupby
+
+
+#ipList = open('topips.txt','r').read()
+
+class Ranking(object):
+  """ Rank the IPs by AS
+  """
+
+  risServer = 'riswhois.ripe.net'
+  whoisPort = 43
+  ASNsHash = {}
   ASNs = []
-  for ip in ips[:]:
-    # Options : http://www.ripe.net/ris/riswhois.html
-    s.send('-k -F -M' + ip + ' \n')
-    data = s.recv(1024)
-    parsedData = re.findall('[0-9./]+',data)
-    if (len(parsedData) >= 2):
-      ASNs.append(parsedData[-2])
-    else:
-      print("bug!! (no associated ASN!) ")
-  s.close()
-  return ASNs
+  ASNsOrdered = []
+  ips = []
 
+  def __init__(self,ipList):
+    self.ips = re.findall('([0-9.]+).+',ipList)
 
-# Idee => http://stackoverflow.com/questions/885546/how-do-you-calculate-the-greatest-number-of-repetitions-in-a-list
-def weightASNs(ASNs):
-  group = groupby (ASNs)
-  result = []
-  index = 0
-  for k, g in group:
-    length = len(list(g))
-    result.append((k, length))
-    index += length
-  sorted(result, key=lambda x:(x[1], x[0]))
-  return result
+  def ASNsofIPs(self):
+    """ Get informations on the AS of each IP 
+    """
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((self.risServer,self.whoisPort))
+    s.recv(1024)
+    for ip in self.ips[:]:
+      # Options : http://www.ripe.net/ris/riswhois.html
+      s.send('-k -M ' + ip + ' \n')
+      data = s.recv(1024)
+      whois = WhoisEntry(data)
+      if not whois.origin:
+        print("No ASN found for " + ip)
+      else: 
+        self.ASNsHash[whois.origin[0]] = whois.description[0]
+        self.ASNs.append(whois.origin[0])
+    s.close()
+    self.ASNs.sort()
 
+  # Original idea => http://stackoverflow.com/questions/885546/how-do-you-calculate-the-greatest-number-of-repetitions-in-a-list
+  def weightASNs(self):
+    """ Rank the ASN and put the description into the list
+    """
+    group = groupby(self.ASNs)
+    index = 0
+    for k, g in group:
+      length = len(list(g))
+      self.ASNsOrdered.append((k, length, self.ASNsHash[k]))
+      index += length
+    self.ASNsOrdered = sorted(self.ASNsOrdered, key=lambda x:(x[1], x[2], x[0]))
 
-# ------------------------ Old fct ---------------------
-
-# return the ASN and the "best network" of this IP 
-def ASNofIP(ip):
-  s = socket(AF_INET, SOCK_STREAM)
-  s.connect((risServer,whoisPort))
-  s.recv(1024)
-  s.send('-F ' + ip + ' \n')
-  data = s.recv(1024)
-  s.close()	
-  # The Last ASN is always the better (has to be verified.) 
-  return re.findall('[0-9./]+',data)[-2:]
---
+++ whoisParser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Original Idea : http://code.google.com/p/pywhois/source/browse/trunk/pywhois/parser.py
+
+import re
+
+class WhoisEntry(object):
+    """Class for parsing a RIS-Whois entry.
+    """
+    _whois_regs = {
+        'route':       '^route:\s*(.+)',
+        'origin':      'origin:\s*AS(.+)',
+        'description': 'descr:\s*(.+)',
+        'first':       'lastupd-frst:\s*(.+)',
+        'last':        'lastupd-last:\s*(.+)',
+        'seen-at':     'seen-at:\s*(.+)',
+        'rispeers':    'num-rispeers:\s*(.+)',
+        'source':      'source:\s*(.+)'
+    }
+
+    def __init__(self, text):
+        self.text = text
+
+
+    def __getattr__(self, attr):
+        """The first time an attribute is called it will be calculated here.
+        The attribute is then set to be accessed directly by subsequent calls.
+        """
+        whois_reg = self._whois_regs.get(attr)
+        if whois_reg:
+            setattr(self, attr, re.findall(whois_reg, self.text))
+            return getattr(self, attr)
+        else:
+            raise KeyError("Unknown attribute: %s" % attr)
+
+    def __str__(self):
+        """Print all whois properties of IP
+        """
+        return '\n'.join('%s: %s' % (attr, str(getattr(self, attr))) for attr in self._whois_regs)

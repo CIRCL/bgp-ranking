b784443482c998737c4b4e10a2dac768e8b8a230
Raphaël Vinot <raphael@vinot.info>
2010-02-24 18:46:16 +0100

    add a function to check if an IP is in a network, works at least with python 2.6

---
+++ ip_manip.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Python 2.6 need an external module : netaddr => http://code.google.com/p/netaddr/
+# Python 2.7 and Python 3.* have already a module : ipaddr
+
+
+from sys import version_info
+if version_info < (2,7):
+  
+  from netaddr import IPAddress, IPNetwork
+
+  def ip_in_network(ip,network):
+    return IPAddress(ip) in IPNetwork(network)
+
+else:
+# TODO ipaddr exists ? => http://docs.python.org/dev/py3k/library/ipaddr.html
+  
+  from ipaddr import IP
+  
+  def ip_in_network(ip,network):
+    return IP(ip) in IP(network)

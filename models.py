adfce2d500d71a367a86d555f9d43ff5cdbfdb44
Raphaël Vinot <raphael@vinot.info>
2010-03-20 22:13:48 +0100

    update of __repr__ in IPs_descriptions and ASNs_descriptions

--- models.py
+++ models.py
@@ -22,13 +22,16 @@ class IPs_descriptions(Entity):
   """ Table which contains a description of the IPs and a link to the ASNs Descriptions 
   """
   list_name = Field(UnicodeText, required=True)
-  fetch_date = Field(DateTime, default=datetime.datetime.now)
+  timestamp = Field(DateTime, default=datetime.datetime.now)
   list_date = Field(DateTime, required=True)
   ip = ManyToOne('IPs')
   asn = ManyToOne('ASNs_descriptions')
   
   def __repr__(self):
-    return 'List Name: "%s"' % (self.list_name)
+    if not self.asn:
+      return '[%s] List: "%s" \t %s' % (self.list_date, self.list_name, self.ip)
+    else:
+      return '[%s] List: "%s" \t %s \t %s' % (self.list_date, self.list_name, self.ip, self.asn.asn)
   
   
   
@@ -38,14 +41,20 @@ class ASNs(Entity):
   asn = Field(Integer, primary_key=True)
   asn_description = OneToMany('ASNs_descriptions')
   
+  def __repr__(self):
+    return 'ASN: "%s"' % (self.asn)
+  
 class ASNs_descriptions(Entity):
   """ Table which contains a description of the ASNs and a link to the IPs Descriptions 
   """
-  seen = Field(DateTime, default=datetime.datetime.now)
-  owner = Field(UnicodeText, required=True)
+  timestamp = Field(DateTime, default=datetime.datetime.now)
+  owner = Field(String, required=True)
   ips_block = Field(Unicode(INET6_ADDRSTRLEN), required=True)
   asn = ManyToOne('ASNs')
   ips = OneToMany('IPs_descriptions')
+  
+  def __repr__(self):
+    return '[%s] %s \t Owner: "%s" ' % (self.timestamp,  self.asn, self.owner)
   
 
 setup_all()

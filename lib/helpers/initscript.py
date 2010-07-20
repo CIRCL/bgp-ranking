6b17c51c4521d532ca5a2439f7cdb3de3d291e79
Raphaël Vinot <raphael.vinot@ses.com>
2010-07-20 16:26:33 +0200

    check correctly if a process is running. more realistic timers

--- lib/helpers/initscript.py
+++ lib/helpers/initscript.py
@@ -96,7 +96,7 @@ def update_running_pids(old_procs):
     """
     new_procs = []
     for proc in old_procs:
-        if check_pid(proc.pid):
+        if proc.poll() == None and check_pid(proc.pid):
             syslog.syslog(syslog.LOG_DEBUG, str(proc.pid) + ' is alive')
             new_procs.append(proc)
         else:

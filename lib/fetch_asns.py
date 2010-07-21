6f2f37086db0cfdded3cab01a289fc74781eda19
Raphaël Vinot <raphael.vinot@ses.com>
2010-07-21 14:29:05 +0200

    sync the config with the adtual config of the server. remove check if the entry exists in a set: sadd do nothing if the entry already exists

--- lib/fetch_asns.py
+++ lib/fetch_asns.py
@@ -107,8 +107,7 @@ class FetchASNs():
         """
         self.ips_descriptions = IPsDescriptions.query.filter(IPsDescriptions.asn==None)[limit_first:limit_last]
         for ip_description in self.ips_descriptions:
-            if not self.cache_db_ris.exists(ip_description.ip.ip):
-                self.temp_db.sadd(config.get('redis','key_temp_ris'),  ip_description.ip.ip)
+            self.temp_db.sadd(config.get('redis','key_temp_ris'),  ip_description.ip.ip)
         while len(self.ips_descriptions) > 0:
             deferred = []
             for description in self.ips_descriptions:
@@ -133,8 +132,7 @@ class FetchASNs():
         """
         self.ips_descriptions = IPsDescriptions.query.filter(IPsDescriptions.whois==None)[limit_first:limit_last]
         for ip_description in self.ips_descriptions:
-            if not self.cache_db_whois.exists(ip_description.ip.ip):
-                self.temp_db.sadd(config.get('redis','key_temp_whois'), ip_description.ip.ip)
+            self.temp_db.sadd(config.get('redis','key_temp_whois'), ip_description.ip.ip)
         while len(self.ips_descriptions) > 0:
             deferred = []
             for description in self.ips_descriptions:

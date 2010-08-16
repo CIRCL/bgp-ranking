0d5ad6651f49d31757ae118a9219a0bc76ea53dc
Raphaël Vinot <raphael.vinot@ses.com>
2010-08-16 18:00:09 +0200

    pushing process finally working

--- bin/services/pushing_process.py
+++ bin/services/pushing_process.py
@@ -7,14 +7,14 @@ config.read("../../etc/bgp-ranking.conf")
 root_dir = config.get('directories','root')
 sys.path.append(os.path.join(root_dir,config.get('directories','libraries')))
 
-
+import redis
 routing_db = redis.Redis(db=config.get('redis','routing_redis_db'))
 
 from whois_parser.bgp_parsers import *
 import syslog
 syslog.openlog('Push_BGP_Routing', syslog.LOG_PID, syslog.LOG_USER)
 
-file = open(fname)
+file = open(sys.argv[1])
 entry = ''
 for line in file:
     if not line:
@@ -30,5 +30,5 @@ for line in file:
             entry = ''
     else :
         entry += line
-syslog.syslog(syslog.LOG_INFO, fname + ' done')
-os.unlink(fname)
+syslog.syslog(syslog.LOG_INFO, sys.argv[1] + ' done')
+os.unlink(sys.argv[1])

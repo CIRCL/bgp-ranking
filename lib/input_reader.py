c00b4ed149e4b5d72e653a44933d93a24ec5e119
Raphaël Vinot <raphael@vinot.info>
2010-09-30 17:29:36 +0200

    uncomment the insertion in the datadase

--- lib/input_reader.py
+++ lib/input_reader.py
@@ -55,13 +55,13 @@ class InputReader():
                 timestamp = datetime.date.today()
             if times is None:
                 times = 1
-            #IP = IPs.query.get(unicode(ip))
-            #if IP is None:
-                #IP = IPs(ip=unicode(ip))
-            #if IPsDescriptions.query.filter_by(ip=IP, list_name=unicode(self.name),\
-                                                #list_date=current_timestamp).first() is None:
-                #IPsDescriptions(ip=IP, list_name=unicode(src), list_date=timestamp, \
-                                #infection=unicode(infection), raw_informations=unicode(raw), times=times)
-            #else:
-                #syslog.syslog(syslog.LOG_ERR, ip + ' already there.')
+            IP = IPs.query.get(unicode(ip))
+            if IP is None:
+                IP = IPs(ip=unicode(ip))
+            if IPsDescriptions.query.filter_by(ip=IP, list_name=unicode(self.name),\
+                                                list_date=current_timestamp).first() is None:
+                IPsDescriptions(ip=IP, list_name=unicode(src), list_date=timestamp, \
+                                infection=unicode(infection), raw_informations=unicode(raw), times=times)
+            else:
+                syslog.syslog(syslog.LOG_ERR, ip + ' already there.')
         return to_return

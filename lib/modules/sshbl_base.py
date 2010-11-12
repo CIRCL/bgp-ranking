3d8da7820f391d9e30526d665ebc726c3f489233
Raphaël Vinot <raphael@vinot.info>
2010-11-12 12:02:44 +0100

    does not match the lines beginning with '#'

--- lib/modules/sshbl_base.py
+++ lib/modules/sshbl_base.py
@@ -23,12 +23,13 @@ class SshblBase(AbstractModule):
         for file in self.files:
             daily = open(file)
             for line in daily:
-                splitted = line.split()
-                ip = splitted[0]
-                date = datetime.utcfromtimestamp(int(splitted[1]))
-                if len(ip) == 0:
-                    continue
-                entry = self.prepare_entry(ip = ip, source = self.__class__.__name__, timestamp = date)
-                self.put_entry(entry)
+                if line[0] != '#':
+                    splitted = line.split()
+                    ip = splitted[0]
+                    date = datetime.utcfromtimestamp(int(splitted[1]))
+                    if len(ip) == 0:
+                        continue
+                    entry = self.prepare_entry(ip = ip, source = self.__class__.__name__, timestamp = date)
+                    self.put_entry(entry)
             daily.close()
             self.move_file(file)

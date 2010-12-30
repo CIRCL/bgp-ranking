4f5347b3d20af05a040d99a09f33722ef87b3ab9
Raphaël Vinot <raphael.vinot@conostix.com>
2010-12-30 21:05:04 +0100

    add a script to stop the redis servers

---
+++ scripts/stop_redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -v
+
+source common.source.sh
+
+OLD_PWD=$PWD
+cd ${REDIS_ROOT}
+
+./redis-cli -p 6379 shutdown
+./redis-cli -p 6380 shutdown
+./redis-cli -p 6381 shutdown
+
+./redis-cli -p 6382 shutdown
+
+
+cd $OLD_PWD

8076f4aaf385aa6b275c317d97cd1ca616fcb826
Raphaël Vinot <raphael.vinot@conostix.com>
2011-01-02 11:38:10 +0100

    update starting scripts redis + config files

--- scripts/stop_redis.sh
+++ scripts/stop_redis.sh
@@ -5,11 +5,10 @@ source common.source.sh
 OLD_PWD=$PWD
 cd ${REDIS_ROOT}
 
-./redis-cli -p 6379 shutdown
-./redis-cli -p 6380 shutdown
-./redis-cli -p 6381 shutdown
-
 ./redis-cli -p 6382 shutdown
 
+./redis-cli -p 6380 shutdown
+./redis-cli -p 6381 shutdown
+./redis-cli -p 6379 shutdown
 
 cd $OLD_PWD

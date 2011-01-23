550d312edcdfd7a1940f7a0781b3774d69111d73
Raphaël Vinot <raphael.vinot@conostix.com>
2011-01-23 14:56:01 +0100

    better usage of the slave instance

--- lib/input_reader.py
+++ lib/input_reader.py
@@ -51,7 +51,7 @@ class InputReader():
         
         table_keys = [ip_key, src_key, timestamp_key, infection_key, raw_key, times_key]
         
-        ip, src, timestamp, infection, raw, times = self.temp_db.mget(table_keys)
+        ip, src, timestamp, infection, raw, times = self.temp_db_slave.mget(table_keys)
         if timestamp is None:
             timestamp = datetime.date.today()
         else:
@@ -62,7 +62,7 @@ class InputReader():
 
     def insert(self):
         to_return = False
-        while self.temp_db_slave.scard(list_ips) > 0:
+        while self.temp_db.scard(list_ips) > 0:
             uid, ip, src, timestamp, infection, raw, times = self.get_all_information()
             if ip is None:
                 syslog.syslog(syslog.LOG_ERR, 'Entry without IP, invalid')

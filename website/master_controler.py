a3aba89c347853b04eb0ad58a590162c242d3165
Raphaël Vinot <raphael@vinot.info>
2011-04-18 00:03:36 -0400

    Revert "display by default the ranking of yesterday"
    This reverts commit 818a8aac92415463a1c952834413959f9a7314f9.

--- website/master_controler.py
+++ website/master_controler.py
@@ -26,7 +26,7 @@ class MasterControler():
         from ranking.reports import Reports
 
         # Ensure there is something to display
-        self.report = Reports(datetime.date.today() - datetime.timedelta(days=1))
+        self.report = Reports(datetime.date.today())
         self.report.flush_temp_db()
         self.report.build_reports_lasts_days(int(self.config.get('ranking','days')))
         self.report.build_reports()

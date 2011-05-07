2586d6a2b643b13c20204041737b2ccf49d9d14d
Raphaël Vinot <raphael@vinot.info>
2011-05-07 17:50:53 -0400

    update microblog bot & reuse the fonctions of the reporting class

---
+++ lib/helpers/common_report.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Common Report
+    ~~~~~~~~~~~~~
+    
+    Mother class of all the modules who are using the reports.
+
+"""
+
+import ConfigParser
+import redis
+
+class CommonReport():
+    """
+        Some values are used quite often. Thanks to this class, the code to get them
+        will not be repeated everywhere.
+    """
+    
+    def __init__(self, ip_version):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.optionxform = str
+        config_file = "/path/to/bgp-ranking.conf"
+        self.config.read(config_file)
+        self.separator = self.config.get('input_keys','separator')
+        self.global_db  = redis.Redis(port = int(self.config.get('redis','port_master')),\
+                                        db = self.config.get('redis','global'))
+        self.history_db = redis.Redis(port = int(self.config.get('redis','port_master')),\
+                                        db = self.config.get('redis','history'))
+        self.history_db_temp = redis.Redis(port = int(self.config.get('redis','port_cache')),\
+                                             db = self.config.get('redis','history'))
+        if ip_version == 4:
+            self.ip_key = self.config.get('input_keys','rankv4')
+        elif ip_version == 6:
+            self.ip_key = self.config.get('input_keys','rankv6')
+
+        self.impacts = {}
+        items = self.config.items('modules_to_parse')
+        for item in items:
+            self.impacts[item[0]] = float(item[1])
+    
+    def get_last_ranking(self):
+        """
+            Get the timestamp of the ranking
+        """
+        return self.history_db.get(self.config.get('ranking','latest_ranking'))
+                            
+    def get_default_date(self):
+        """
+            Set the default date displayed on the website.
+        """
+        timestamp = self.get_last_ranking()
+        if timestamp is not None:
+            timestamp = timestamp.split()
+            default_date_raw = dateutil.parser.parse(timestamp[0]).date() - datetime.timedelta(days=1)
+        else:
+            default_date_raw = datetime.date.today() - datetime.timedelta(days=1)
+        default_date = default_date_raw.isoformat()
+        return default_date_raw, default_date
+
+    def get_sources(self, date):
+        """
+            Get the sources parsed on a `date`
+        """
+        return self.global_db.smembers('{date}{sep}{key}'.format(  date   = date, \
+                            sep    = self.separator,\
+                            key    = self.config.get('input_keys','index_sources')))
+
+    def get_dates(self):
+        """
+            Get the dates where there is a ranking available in the database
+        """
+        return self.history_db_temp.smembers(self.config.get('ranking','all_dates'))
+
+    
+    def get_daily_rank(self, asn, source = None, date = None):
+        """
+            Get the rank of an AS for a particular `source` and `date`
+        """
+        if source is None:
+            source = self.config.get('input_keys','histo_global')
+        return self.history_db.get(\
+                    '{asn}{sep}{date}{sep}{source}{sep}{ip_key}'.format(sep     = self.separator,\
+                                                                        asn     = asn,\
+                                                                        date    = date,\
+                                                                        source  = source,\
+                                                                        ip_key  = self.ip_key))
+    
--- lib/microblog/micro_blog.py
+++ lib/microblog/micro_blog.py
@@ -1,29 +1,68 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+"""
+    Micro-blog
+    ~~~~~~~~~~~~~
+    
+    Microblog client for twitter and identica
+"""
+
 import twitter
 from micro_blog_keys import *
+from helpers.common_values import CommonValues
 
-class MicroBlog():
+class MicroBlog(CommonReport):
     """
         Twitter client which inform the followers when something happens
         on BGP Ranking
     """
     
-    def __init__(self):
-        self.twitter_api = twitter.Api( consumer_key        =   twitter_customer_key,
-                                        consumer_secret     =   twitter_consumer_secret,
-                                        access_token_key    =   twitter_access_token_key,
-                                        access_token_secret =   twitter_access_token_secret)
-        self.identica_api = twitter.Api(consumer_key        =   identica_customer_key,
-                                        consumer_secret     =   identica_consumer_secret,
-                                        access_token_key    =   identica_access_token_key,
-                                        access_token_secret =   identica_access_token_secret,
-                                        base_url = 'https://identi.ca/api')
+    def __init__(self, ip_version = 4):
+        CommonReport.__init__(self, ip_version)
 
     def post(self, string):
+        """
+            Post an entry in twitter and identi.ca
+        """
         if len(string) <= 140:
             self.twitter_api.PostUpdate(string)
             self.identica_api.PostUpdate(string)
             return 0
         return 1
+    
+    def top_date(self, date = None):
+        """
+            Return the top ASN for a given date
+        """
+        if date is None:
+            raw_date, date = self.get_default_date()
+        source = self.config.get('input_keys','histo_global')
+        histo_key = '{date}{sep}{histo_key}{sep}{ip_key}'.format(   sep         = self.separator,\
+                                                                    date        = date,\
+                                                                    histo_key   = source,\
+                                                                    ip_key      = self.ip_key)
+        limit = 5
+        reports = self.history_db_temp.zrevrange(histo_key, 0, limit, True)
+        to_return = None
+        if reports is not None:
+            to_return = ''
+            for report in reports:
+                to_return.join('{asn}: {rank}\n'.format(asn = report[0], rank = report[1]))
+        return to_return
+
+    def last_ranks_asn(self, asn):
+        """
+            Return all the rankings found in the DB for a given ASN
+        """
+        dates = common_values.get_dates()
+        ranks = []
+        values = ''
+        for date in dates:
+            rank = self.get_daily_rank(asn = asn, source = None, date = date)
+            if rank is not None:
+                values.join('{date}: {rank}\n'.format(date = date, rank = rank))
+        if len(values) > 0:
+            return '{asn}\n{values}'.format(asn = asn, values = values)
+        return None

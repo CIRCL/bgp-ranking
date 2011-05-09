f3d4fe294b79a80febcf67a85037930a39a0d8bb
Raphaël Vinot <raphael@vinot.info>
2011-05-09 14:52:26 -0400

    display the rank of a block on the web interface

--- lib/ranking/reports.py
+++ lib/ranking/reports.py
@@ -290,7 +290,8 @@ class Reports(CommonReport):
                                                                         ip_block = self.config.get('input_keys','ips_block'))]
                 owner, ip_block = self.global_db.mget(keys)
                 sources_web = self.history_db_temp.smembers(asn_timestamp_temp)
-                asn_descs_to_print.append([asn, timestamp, owner, ip_block, nb_of_ips, ', '.join(sources_web)])
+                asn_descs_to_print.append([asn, timestamp, owner, ip_block, nb_of_ips,\
+                                            ', '.join(sources_web), 1 + nb_of_ips / IPy.IP(ip_block).len()])
         to_return = sorted(asn_descs_to_print, key=lambda desc: IP(desc[3]).len())
         return to_return, current_asn_sources
 

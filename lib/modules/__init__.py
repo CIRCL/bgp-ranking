a92a32c0a62cefa4e4480e284c242742113dc85b
Raphaël Vinot <raphael@vinot.info>
2011-05-18 16:06:33 -0400

    try a better way to import the modules in the parser service

--- lib/modules/__init__.py
+++ lib/modules/__init__.py
@@ -1,7 +1,9 @@
-from abuse_ch_lists import AmadaIpBlockList, SpyeyetrackerIpBlockList, ZeustrackerIpBlockList, SpyeyetrackerDdos,ZeustrackerDdos
-from blocklist_de import BlocklistDeSsh, BlocklistDeMail, BlocklistDeApache, BlocklistDePop3, BlocklistDeFtp
-from simple_lists import URLQuery, MalwareDomainListIP, DshieldTopIPs
+# Source : http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python/1057765#1057765
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
 
-from dshield_daily import DshieldDaily
-from malc0de import Malc0de
-from sshbl_base import  SshblBase
+# http://stackoverflow.com/questions/547829/how-to-dynamically-load-a-python-class

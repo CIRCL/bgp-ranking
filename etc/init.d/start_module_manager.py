72c9aa605f9f7904aec1c4ccad51abe0cb0ba2dd
Raphaël Vinot <raphael@vinot.info>
2011-05-18 01:30:30 -0400

    add two forgotten files

---
+++ etc/init.d/start_module_manager.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Inspired by : http://gitorious.org/forban/forban/blobs/master/bin/forbanctl
+
+"""
+Start the module manager
+"""
+import os 
+import sys
+import ConfigParser
+
+import signal
+import syslog
+
+def usage():
+    print "start_module_manager.py (start|stop)"
+    exit (1)
+
+
+if __name__ == '__main__':
+
+    config = ConfigParser.RawConfigParser()
+    config_file = "/path/to/bgp-ranking.conf"
+    config.read(config_file)
+    root_dir = config.get('directories','root')
+    sys.path.append(os.path.join(root_dir,config.get('directories','libraries')))
+    from helpers.initscript import *
+    services_dir = os.path.join(root_dir,config.get('directories','services'))
+
+    syslog.openlog('BGP_Ranking_ModuleManager', syslog.LOG_PID, syslog.LOG_LOCAL5)
+
+    if len(sys.argv) < 2:
+        usage()
+    service = os.path.join(services_dir, "module_manager")
+
+    if sys.argv[1] == "start":
+        print("Starting ModuleManager...")
+        syslog.syslog(syslog.LOG_INFO, "Starting ModuleManager...")
+        print(service+" to start...")
+        syslog.syslog(syslog.LOG_INFO, service+" to start...")
+        proc = service_start_once(servicename = service, processname = service)
+
+    elif sys.argv[1] == "stop":
+        print("Stopping ModuleManager...")
+        syslog.syslog(syslog.LOG_INFO, "Stopping ModuleManager...")
+        pid = pidof(processname=service)
+        if pid:
+            pid = pid[0]
+            try:
+                os.kill(int(pid), signal.SIGHUP)
+            except OSError, e:
+                print(service+  " unsuccessfully stopped")
+                syslog.syslog(syslog.LOG_ERR, service+  " unsuccessfully stopped")
+            rmpid(processname=service)
+        else:
+            print('No running ModuleManager process')
+            syslog.syslog(syslog.LOG_INFO, 'No running ModuleManager process')
+    else:
+        usage()
+
---
+++ scripts/add_modules.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import ConfigParser
+import redis
+
+class AddModules(object):
+    
+    
+    def __init__(self):
+        config_file = "/path/to/bgp-ranking.conf"
+        config_file_redis = config_file + ".redis"
+
+        config = ConfigParser.SafeConfigParser()
+        config.read(config_file)
+
+        self.config_db  = redis.Redis( port = int(config.get('redis','port_master')),\
+                                    db = int(config.get('redis','config')))
+
+    def push_module_information(self, module, impact, home_dir = None, url = None):
+        self.config_db.set(module, impact)
+        if home_dir is not None:
+            self.config_db.set(module + "|" + "home_dir", home_dir)
+        if url is not None:
+            self.config_db.set(module + "|" + "url", home_dir)
+
+        # !!! Always last !!!!
+        self.config_db.sadd('modules', name)
+
+
+    
+if __name__ == '__main__':
+    # Make it a "ncurse interface"
+    config_file = "/path/to/bgp-ranking.conf"
+    config_file_redis = config_file + ".redis"
+    
+    redis_config = ConfigParser.RawConfigParser()
+    redis_config.optionxform = str
+    redis_config.read(config_file_redis)
+    
+    add_modules = AddModules()
+    
+    items = redis_config.items('modules')
+    for module, params in items:
+        p = params.split()
+        add_modules.push_module_information(module, *p)

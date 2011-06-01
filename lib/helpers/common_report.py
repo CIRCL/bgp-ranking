0b3aeef9ccb28d1d20336068ef6a1eef8622f0b5
Raphaël Vinot <raphael@vinot.info>
2011-06-01 13:48:49 -0400

    automatically sort the dates and the sources

--- lib/helpers/common_report.py
+++ lib/helpers/common_report.py
@@ -60,16 +60,16 @@ class CommonReport(object):
         """
             Get the dates where there is a ranking available in the database
         """
-        return self.history_db_temp.smembers(self.config.get('ranking','all_dates'))
+        return sorted(self.history_db_temp.smembers(self.config.get('ranking','all_dates')))
 
     def get_sources(self, date):
         """
             Get the sources parsed on a `date`
         """
-        return self.global_db.smembers('{date}{sep}{key}'.format(\
+        return sorted(self.global_db.smembers('{date}{sep}{key}'.format(\
                             date   = date, \
                             sep    = self.separator,\
-                            key    = self.config.get('input_keys','index_sources')))
+                            key    = self.config.get('input_keys','index_sources'))))
 
     def get_multiple_daily_rank(self, asn_list, date, source):
         string = '{sep}{date}{sep}{source}{sep}{ip_key}'.format(sep    = self.separator,\

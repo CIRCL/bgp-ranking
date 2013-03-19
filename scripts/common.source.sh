1ef0212bba17be685120f160ac3c5d03fe5d000d
Raphaël Vinot <raphael@vinot.info>
2013-03-19 16:34:18 +0100

    Cleanup and update init scripts.

--- scripts/common.source.sh
+++ scripts/common.source.sh
@@ -2,12 +2,10 @@ set -e
 
 PREFIX="${HOME}/gits/"
 BGP_RANKING_ROOT="${PREFIX}bgp-ranking"
-#PYTHON="${PREFIX}/Python-2.7/python"
 PYTHON="/usr/bin/python"
-#NICE="nice -n15 ionice -n2 -c7"
 NICE=""
 
-REDIS_ROOT="/home/raphael/gits/redis/src/"
+REDIS_ROOT="${PREFIX}redis/src/"
 
 REDIS_SERVER="./redis-server"
 REDIS_CLIENT="./redis-cli"

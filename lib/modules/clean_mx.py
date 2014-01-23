55111591b5ad30a67b26fcdcbfa0c35b30b246ef
Raphaël Vinot <raphael@vinot.info>
2014-01-23 09:40:08 +0100

    make sure the clean mx entries only appears once

--- lib/modules/clean_mx.py
+++ lib/modules/clean_mx.py
@@ -19,11 +19,11 @@ def parser(filename, listname, date):
         Parse the list
     """
     a, b, year, month, day, hour, c = os.path.basename(filename).split('.')
-    date = datetime.datetime(int(year), int(month), int(day), int(hour))
     with open(filename, 'r') as f :
         for line in f:
             ip = re.findall('<(?:ip|review)>((?:\d{1,3}\.){3}\d{1,3})<.*',line)
             if len(ip) == 0:
                 continue
             new_entry(ip = ip[0], source = listname, timestamp = date)
+    date = datetime.datetime(int(year), int(month), int(day), int(hour))
     return date
